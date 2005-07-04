8f61ee680badfdd4bad9d533f2109cd1d9a2c123
Matthieu.Sozeau <Matthieu.Sozeau@lri.fr>
2005-07-04 15:05:44 +0000

    Slides, handout

---
+++ papers/slides/make_handout.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+if [ $# -eq 0 ]
+then
+	exit
+fi
+
+pdfnup --turn true --noautoscale false --nup $1 --outfile slides-handout-$1.pdf slides-handout.pdf
+pdf90 slides-handout-$1.pdf
+pdftops -level3 -expand slides-handout-$1-rotated.pdf slides-handout-$1.ps
+rm -f slides-handout-{$1}*.pdf
