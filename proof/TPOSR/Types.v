e216baa8ac0bf3530be9298c20261fc4245730ea
Matthieu Sozeau <mattam@mattam.org>
2006-05-22 10:01:29 +0000

    Advance TPOSR metatheory. Have to clarify which systems we use, next is substitution, weakening et al

--- proof/TPOSR/Types.v
+++ proof/TPOSR/Types.v
@@ -9,6 +9,8 @@ Require Import TPOSR.Conv.
 Require Import TPOSR.LiftSubst.
 Require Import TPOSR.Env.
 
+Set Implicit Arguments.
+
 Reserved Notation "G |- T -> U : s" (at level 70, T, U, s at next level).
 
 Definition sum_sort s1 s2 s3 :=
@@ -51,8 +53,8 @@ with tposr : lenv -> lterm -> lterm -> lterm -> Prop :=
   forall B s, e |- A -> B : Srt_l s ->
   e |- M -> N : B
   
-  | tposr_exp : forall e M N A, e |- M -> N : A -> 
-  forall B s, e |- A -> B : Srt_l s ->
+  | tposr_exp : forall e M N B, e |- M -> N : B -> 
+  forall A s, e |- A -> B : Srt_l s ->
   e |- M -> N : A
   
   | tposr_subset : forall e A A', e |- A -> A' : Srt_l set ->
@@ -90,5 +92,59 @@ with tposr : lenv -> lterm -> lterm -> lterm -> Prop :=
   e |- Pi2_l (Pair_l (Sum_l A B) u v) -> v : lsubst u B
 
 where "G |- T -> U : s" := (tposr G T U s).
+
+Inductive tposrp : lenv -> lterm -> lterm -> lterm -> Prop :=
+  | tposrp_tposr : forall e X Y Z, e |- X -> Y : Z -> tposrp e X Y Z
+  | tposrp_trans : forall e W X Y Z, tposrp e W X Z -> tposrp e X Y Z ->
+  tposrp e W Y Z.
+
+Reserved Notation "G |- T ~= U : s" (at level 70, T, U, s at next level).
+
+Inductive tposr_eq : lenv -> lterm -> lterm -> sort -> Prop :=
+  | tposr_eq_tposr : forall e X Y s, e |- X -> Y : Srt_l s -> e |- X ~= Y : s
+  | tposr_eq_sym : forall e X Y s, e |- X ~= Y : s -> e |- Y ~= X : s
+  | tposr_eq_trans : forall e W X Y s, e |- W ~= X : s -> e |- X ~= Y : s ->
+  e |- W ~= Y : s
+
+where "G |- T ~= U : s" := (tposr_eq G T U s).
+
+Lemma tposr_conv : forall e A B s, e |- A ~= B : s -> 
+  forall M N, (e |- M -> N : A -> e |- M -> N : B) /\ (e |- M -> N : B -> e |- M -> N : A).
+Proof.
+  induction 1 ; simpl ; intros.
+  
+  split ; intros.
+  apply tposr_red with X s ; auto.
+  apply tposr_exp with Y s ; auto.
+
+  pose (IHtposr_eq M N).
+  intuition ; auto with coc.
+
+  pose (IHtposr_eq1 M N).
+  pose (IHtposr_eq2 M N).
+  intuition ; auto with coc.
+Qed.
+
+Lemma tposr_lred : forall e M N Z, e |- M -> N : Z -> lred M N.
+Proof.
+  induction 1 ; simpl ; auto with coc.
+
+  apply trans_lred with (App_l B' (Abs_l A' M') N') ; auto with coc.
+Qed.
+
+Lemma tposr_eq_conv : forall e M N Z, e |- M ~= N : Z -> conv M N.
+Proof.
+  induction 1 ; simpl ; auto with coc.
+  
+  pose (tposr_lred H) ; auto with coc.
+  apply trans_conv_conv with X ; auto with coc.
+Qed.
+
+Lemma wf_tposr : forall e M N T, e |- M -> N : T -> tposr_wf e.
+Proof.
+  induction 1 ; simpl ; auto with coc.
+Qed.
+
+
 
   
