3fac713ba9e7d809faaf3c4f2a94f54444b16241
Matthieu Sozeau <mattam@mattam.org>
2006-05-31 13:01:30 +0000

    Removed old files, better doc support

--- proof/COC/ImpVar.v
+++
@@ -1,4 +0,0 @@
- Implicit Types i k m n p : nat.
-Implicit Type s : sort.
-Implicit Types A B M N T t u v : term.
-Implicit Types e f g : env.
--- proof/makedoc.sh
+++ proof/makedoc.sh
@@ -1,4 +1,4 @@
 #/bin/bash
 SOURCES=`find . -name "*.v"`
 echo ${SOURCES}
-coqdoc -g --toc -d html --html ${SOURCES} 
+coqdoc --coqlib_path "`coqc -where`" --multi-index --glob-from globals.dump -g -l -t "Russell metatheoretic study" --toc -d html --html ${SOURCES} 
