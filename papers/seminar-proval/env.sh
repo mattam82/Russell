ba9c8b882920a094ff169bb71733b96efa083be6
Matthieu Sozeau <mattam@mattam.org>
2007-03-02 14:39:20 +0000

    Prepare new slides

---
+++ papers/seminar-proval/env.sh
@@ -0,0 +1,3 @@
+export TEXINPUTS=..:../style:../figures:${HOME}/research/publication/styles:${TEXINPUTS}
+export BIBINPUTS=${HOME}/research/demons-biblio:../bib:${BIBINPUTS}
+export BSTINPUTS=../style:${BSTINPUTS}
