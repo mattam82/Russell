fa170db6df4769931489daf7fc73ecfd5fb7c965
Matthieu Sozeau <mattam@mattam.org>
2008-02-01 05:40:21 +0000

    Better doc, don't put hnf in it

--- proof/makedoc.sh
+++ proof/makedoc.sh
@@ -3,3 +3,5 @@ SOURCES=`grep .v Make`
 echo ${SOURCES}
 rm html/*
 coqdoc --coqlib_path "`coqc -where`" --multi-index --glob-from globals.dump -g -l -t "Russell metatheoretic study" --toc -d html --html ${SOURCES} 
+rm html/coqdoc.css
+ln -s ~/research/publication/styles/coqdoc.css html
